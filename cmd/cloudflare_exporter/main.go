5df20ec3fae094c6f3cb4f0fecf9bdc208fe82d3
Panagiotis Atmatzidis <atma@convalesco.org>
2021-04-29 13:59:29 +0300

    [feat] requests package implementation
    Add golang package to perform graphQL queries.

--- cmd/cloudflare_exporter/main.go
+++ cmd/cloudflare_exporter/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	config "github.com/gathertown/cloudflare_exporter/internal/config"
+	r "github.com/gathertown/cloudflare_exporter/pkg/cloudflare/requests"
 	log "github.com/gathertown/cloudflare_exporter/pkg/log"
 )
 
@@ -12,4 +14,9 @@ func main() {
 	cfg := config.FromEnv()
 	logger := log.New(os.Stdout, cfg.Env)
 	logger.Info("Launching cloudformation_exporter", "zoneTag", cfg.ZoneTag)
+	a, err := r.Requests(cfg.ZoneTag, cfg.Token)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(a)
 }
--- pkg/cloudflare/requests/main.go
+++ pkg/cloudflare/requests/main.go
@@ -1,97 +1,136 @@
-// Github Crawler golang package fetches list of repos featuring a topic, returns a map
-
 package requests
 
 import (
 	"context"
 	"fmt"
+	"os"
+	"time"
 
-	"github.com/shurcooL/githubv4"
+	"github.com/gathertown/cloudflare_exporter/internal/config"
+	log "github.com/gathertown/cloudflare_exporter/pkg/log"
+	graphql "github.com/shurcooL/graphql"
 	"golang.org/x/oauth2"
 )
 
-// Crawler struct defines the fields needed by ghcrawler pkg
-type Crawler struct {
-	PageItems int    // The number of items to fetch per API call, the limit is 100
-	Org       string // Github organisation
-	Token     string // Github token
-	Topic     string // Github topic
+var cfg = config.FromEnv()
+var logger = log.New(os.Stdout, cfg.Env)
+
+const endpoint = "https://api.cloudflare.com/client/v4/graphql"
+
+// start graphQL structs
+
+type countryMap []struct {
+	ClientCountryName graphql.String
+	Requests          graphql.Int
+	Threats           graphql.Int
+	Bytes             graphql.Int
 }
 
-/* GraphQL struct start */
-type pageinfo struct {
-	HasNextPage bool
-	EndCursor   string // When 'HasNextPage' is 'true', EndCursor will return the page number to fetch next
+type browserMap []struct {
+	UaBrowserFamily graphql.String
+	PageViews       graphql.Int
 }
 
-type branch struct {
-	Name string
+type responseStatusMap []struct {
+	Requests           graphql.Int
+	EdgeResponseStatus graphql.Int
 }
 
-type repository struct {
-	DefaultBranchRef branch
-	Name             string // Github repository name
-	URL              string // Github repository URL
-	ID               string // Github uses an alphanumeric string as a repository unique identifier
+type httpRequests1mGroupsSum struct {
+	CountryMap        countryMap
+	BrowserMap        browserMap
+	ResponseStatusMap responseStatusMap
 }
 
-type repos []struct {
-	Repository repository `graphql:"... on Repository"`
+type dimensions struct {
+	DatetimeMinute graphql.String
 }
 
-type search struct {
-	PageInfo pageinfo // Contains info about graphQL API pagination
-	Repos    repos    `graphql:"repo: nodes"`
+type httpRequests1mGroups []struct {
+	Dimensions dimensions
+	Sum        httpRequests1mGroupsSum
 }
 
-func client(ctx context.Context, token string) *githubv4.Client {
-	ts := oauth2.StaticTokenSource(
+// NOTE:
+// "unique visitors" per colocation is not supported in httpRequestsAdaptiveGroups,
+// but the httpRequestsAdaptiveGroups API does support visits.
+// A visit is defined as a page view that originated from a different website or direct link.
+// Cloudflare checks where the HTTP referer does not match the hostname. One visit can consist of multiple page views.
+//
+// URL: https://developers.cloudflare.com/analytics/graphql-api/migration-guides/graphql-api-analytics
+type httpRequestsAdaptiveGroupsSum struct {
+	Visits            graphql.Int
+	EdgeResponseBytes graphql.Int
+}
+
+type httpRequestsAdaptiveGroups []struct {
+	Dimensions dimensions
+	Sum        httpRequestsAdaptiveGroupsSum
+}
+
+type zones []struct {
+	HttpRequests1mGroups       httpRequests1mGroups       `graphql:"httpRequests1mGroups(filter: {datetime_geq: $dateTimeStart, datetime_leq: $dateTimeEnd}, orderBy: [ datetimeMinute_ASC], limit: 1)"`
+	HttpRequestsAdaptiveGroups httpRequestsAdaptiveGroups `graphql:"httpRequestsAdaptiveGroups(filter: {datetime_geq: $dateTimeStart, datetime_leq: $dateTimeEnd}, orderBy: [ datetimeMinute_ASC], limit: 1)"`
+}
+
+var q struct {
+	Viewer struct {
+		Zones zones `graphql:"zones(filter: {zoneTag: $zoneTag})"`
+	}
+}
+
+func Requests(zoneTag string, token string) (int, error) {
+	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
 	)
-	tc := oauth2.NewClient(ctx, ts)
-	return githubv4.NewClient(tc)
-}
+	httpClient := oauth2.NewClient(context.Background(), src)
 
-/* GraphQL struct end */
+	client := graphql.NewClient(endpoint, httpClient)
 
-// Fetch gathers repositories from github featuring a topic. Returns the names of the repositories as a slice of strings.
-func (c Crawler) Fetch(ctx context.Context) ([]map[string]string, error) {
-	// Github API v4 requires an API Token:
-	// https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
-	clnt := client(ctx, c.Token)
-
-	// GraphQL does not support string concatenation. We'll provide the query as a pre-constructed string instead.
-	// https://github.com/shurcooL/githubv4/issues/19#issuecomment-326034518
-	query := fmt.Sprintf("topic:%s org:%s", c.Topic, c.Org)
-
-	variables := map[string]interface{}{
-		"items":  githubv4.Int(c.PageItems),
-		"query":  githubv4.String(query),
-		"cursor": (*githubv4.String)(nil),
+	// Start duration
+	d1, err := time.ParseDuration("-3m")
+	if err != nil {
+		return 0, err
 	}
 
-	var q struct {
-		Search search `graphql:"search(first: $items, after: $cursor type: REPOSITORY, query: $query)"`
+	// Stop duration
+	d2, err := time.ParseDuration("-2m")
+	if err != nil {
+		return 0, err
 	}
-	var repos []map[string]string
+	t1 := time.Now().UTC().Add(d1).Format(time.RFC3339)
+	t2 := time.Now().UTC().Add(d2).Format(time.RFC3339)
 
-	for {
-		err := clnt.Query(ctx, &q, variables)
-		if err != nil {
-			return nil, err
-		}
-		for _, repo := range q.Search.Repos {
-			m := make(map[string]string)
-			m["name"] = repo.Repository.Name
-			m["id"] = repo.Repository.ID
-			m["url"] = repo.Repository.URL
-			m["branch"] = repo.Repository.DefaultBranchRef.Name
-			repos = append(repos, m)
-		}
-		if !q.Search.PageInfo.HasNextPage {
-			break
-		}
-		variables["cursor"] = githubv4.String(q.Search.PageInfo.EndCursor)
+	opts := map[string]interface{}{
+		"dateTimeStart": graphql.String(t1),
+		"dateTimeEnd":   graphql.String(t2),
+		"zoneTag":       graphql.String(zoneTag),
 	}
-	return repos, nil
+
+	err = client.Query(context.Background(), &q, opts)
+	if err != nil {
+		return 0, err
+	}
+
+	logger.Debug("time", "dateTimeStart", t1)
+	logger.Debug("time", "dateTimeEnd", t2)
+
+	for _, k := range q.Viewer.Zones {
+		for _, v := range k.HttpRequestsAdaptiveGroups {
+			fmt.Printf("DateTime: %s, Visits: %v, Bytes: %v\n", v.Dimensions.DatetimeMinute, v.Sum.Visits, v.Sum.EdgeResponseBytes)
+		}
+		for _, v := range k.HttpRequests1mGroups {
+			fmt.Printf("DateTime: %s\n", v.Dimensions.DatetimeMinute)
+			for _, d := range v.Sum.ResponseStatusMap {
+				fmt.Printf("status code: %v, requests: %v\n", d.EdgeResponseStatus, d.Requests)
+			}
+			for _, b := range v.Sum.BrowserMap {
+				fmt.Printf("BrowserFamily: %v, PageViews: %v\n", b.UaBrowserFamily, b.PageViews)
+			}
+			for _, c := range v.Sum.CountryMap {
+				fmt.Printf("Country: %v, Bytes: %v, Requests: %v, Threats: %v\n", c.ClientCountryName, c.Bytes, c.Requests, c.Threats)
+			}
+		}
+	}
+	return 1, nil
 }
